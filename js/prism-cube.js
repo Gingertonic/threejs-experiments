3b93f6087cbed5f88e2f89995e50bc044d210299
Gingertonic <bethmschofield@gmail.com>
2018-11-11 20:34:43 +0000

    tidy code

--- js/prism-cube.js
+++ js/prism-cube.js
@@ -17,6 +17,7 @@ window.addEventListener('resize', function(){
 })
 
 
+// Similar method to below to creat array of images
 const loader = new THREE.CubeTextureLoader();
 loader.setPath( 'images/' );
 
@@ -27,103 +28,58 @@ const textureCube = loader.load( [
 ] );
 
 
-// mixed type face materials
-const grottoMaterials = [
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/grotto.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/grotto.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/grotto.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/grotto.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/grotto.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/grotto.png'), side: THREE.DoubleSide } )
+// Make arrays of images to create new Texture
+const grottoMaterials = new Array(6).fill(new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/grotto.png'), side: THREE.DoubleSide } ))
+const scaleThisMaterials = new Array(6).fill(new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/scalethis.png'), side: THREE.DoubleSide } ))
+const inspauralMaterials = new Array(6).fill(new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/inspaural.png'), side: THREE.DoubleSide } ))
+const bvMaterials = new Array(6).fill(new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/bv.png'), side: THREE.DoubleSide } ))
 
-  // new THREE.MeshPhongMaterial( { color: 0x000000, side: THREE.BackSide }),
-  // new THREE.MeshPhongMaterial( { color: 0x000000, side: THREE.BackSide }),
-  // new THREE.MeshPhongMaterial( { color: 0x000000, side: THREE.BackSide }),
-  // new THREE.MeshPhongMaterial( { color: 0x000000, side: THREE.BackSide })
-]
-
-const scaleThisMaterials = [
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/scalethis.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/scalethis.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/scalethis.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/scalethis.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/scalethis.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/scalethis.png'), side: THREE.DoubleSide } )
-
-  // new THREE.MeshPhongMaterial( { color: 0xADF442 }),
-  // new THREE.MeshPhongMaterial( { color: 0xADF442 }),
-  // new THREE.MeshPhongMaterial( { color: 0xF4AD42}),
-  // new THREE.MeshPhongMaterial( { color: 0xF4AD42 })
-]
-
-const inspauralMaterials = [
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/inspaural.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/inspaural.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/inspaural.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/inspaural.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/inspaural.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/inspaural.png'), side: THREE.DoubleSide } )
-  //
-  // new THREE.MeshPhongMaterial( { color: 0x000000 }),
-  // new THREE.MeshPhongMaterial( { color: 0x000000 }),
-  // new THREE.MeshPhongMaterial( { color: 0x000000}),
-  // new THREE.MeshPhongMaterial( { color: 0x000000 })
-]
-
-const bvMaterials = [
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/bv.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/bv.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/bv.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/bv.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/bv.png'), side: THREE.DoubleSide } ),
-  new THREE.MeshPhongMaterial( { map: new THREE.TextureLoader().load('images/bv.png'), side: THREE.DoubleSide } )
-  //
-  // new THREE.MeshPhongMaterial( { color: 0x000000 }),
-  // new THREE.MeshPhongMaterial( { color: 0x000000 }),
-  // new THREE.MeshPhongMaterial( { color: 0x000000}),
-  // new THREE.MeshPhongMaterial( { color: 0x000000 })
-]
-
-// define item shape - width, depth, height
+// Define required geometries(width, depth, height)
 const geometry = new THREE.BoxGeometry( 3, 3, 3 );
 const smallGeometry = new THREE.BoxGeometry( 1.5, 1.5, 1.5 );
-// define item material with image array
+
+// Define Materials
 const prismMaterial = new THREE.MeshLambertMaterial( { color: 0xffffff, envMap: textureCube } );
-// define item material with mixed material array
 const p1Material = new THREE.MeshFaceMaterial( grottoMaterials );
 const p2Material = new THREE.MeshFaceMaterial( scaleThisMaterials );
 const p3Material = new THREE.MeshFaceMaterial( inspauralMaterials );
 const p4Material = new THREE.MeshFaceMaterial( bvMaterials );
-// put them together to create a green cube
+
+// Put together the geometries and materials to create the cubes
 const cube = new THREE.Mesh( geometry, prismMaterial );
 const p1Cube = new THREE.Mesh( smallGeometry, p1Material );
 const p2Cube = new THREE.Mesh( smallGeometry, p2Material );
 const p3Cube = new THREE.Mesh( smallGeometry, p3Material );
 const p4Cube = new THREE.Mesh( smallGeometry, p4Material );
 
+
+//Cube starting positions
 // cube.rotation.x = 0.5;
 cube.rotation.y = 0.5;
 p1Cube.position.x = 5;
-p1Cube.position.z = 3;
+p1Cube.position.x = 5;
+p1Cube.rotation.z = 0.5;
 p4Cube.position.x = -5;
 p4Cube.position.z = -3;
+p4Cube.rotation.z = 0.3;
 p3Cube.position.y = 4;
 p3Cube.position.z = 3;
+p3Cube.rotation.y = 0.7;
 p2Cube.position.y = -4;
 p2Cube.position.z = -3;
+p3Cube.rotation.y = 0.4;
 
 
-// add the cube to the scene
+// add the cubes to the scene
 scene.add( cube );
 scene.add( p1Cube );
 scene.add( p2Cube );
 scene.add( p3Cube );
 scene.add( p4Cube );
 
-// CUBE HAS BEEN MADE
 
 // ADD LIGHTING
-const ambientLight = new THREE.AmbientLight(0xFFF7e2, 0.5)
+const ambientLight = new THREE.AmbientLight(0xFFF7e2, 0.6)
 scene.add(ambientLight)
 
 const spotLight = new THREE.SpotLight(0xFFFFFF, 1.3)
@@ -136,7 +92,7 @@ scene.add(spotLight)
 
 
 // place the camera
-camera.position.z = 8;
+camera.position.z = 10;
 
 // basic orbit controls
 const controls = new THREE.OrbitControls(camera, renderer.domElement)
@@ -166,7 +122,6 @@ const renderScene = () => {
 }
 
 // actually render the scene with 'render loop' aka 'animate loop'
-// anything you want to move or change while the app is running has to go through the animate loop.
 function animate() {
   requestAnimationFrame(animate);
   update()
